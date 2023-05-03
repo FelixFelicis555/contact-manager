09528986cb762b250818f8e384cb9c6d2014452c
FelixFelicis555 <perfectpawi13@gmail.com>
2023-05-03 15:29:35 +0530

    Updates

---
+++ precache-manifest.aaa9d3aca48cb12bf4875b404fd15201.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "27a6de29e2ece2dde249",
+    "url": "/contact-manager/static/css/main.e55b0db8.chunk.css"
+  },
+  {
+    "revision": "27a6de29e2ece2dde249",
+    "url": "/contact-manager/static/js/main.27a6de29.chunk.js"
+  },
+  {
+    "revision": "d7adfc5fbfde727debc7",
+    "url": "/contact-manager/static/js/1.d7adfc5f.chunk.js"
+  },
+  {
+    "revision": "2b8fdbf7579a38197298",
+    "url": "/contact-manager/static/js/runtime~main.2b8fdbf7.js"
+  },
+  {
+    "revision": "7ea9958e269b638fdc89a3c1eee6d3b1",
+    "url": "/contact-manager/index.html"
+  }
+];
